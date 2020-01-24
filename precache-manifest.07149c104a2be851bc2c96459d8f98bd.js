56d2f1e7dfd6d37c8054597954c428b3359da205
Saif <mujawarmsaif@gmail.com>
2020-01-24 18:16:21 +0530

    Updates

---
+++ precache-manifest.07149c104a2be851bc2c96459d8f98bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84bd10ea85ea137a04dcfcf0ae8067c",
+    "url": "/adminwebsite/index.html"
+  },
+  {
+    "revision": "ade6d194e6c642d9566d",
+    "url": "/adminwebsite/static/css/main.bd8560cd.chunk.css"
+  },
+  {
+    "revision": "fa5c69f70952a63220a4",
+    "url": "/adminwebsite/static/js/2.bc77d50f.chunk.js"
+  },
+  {
+    "revision": "86cf7f074fde330b07dbd8e5fbabf90d",
+    "url": "/adminwebsite/static/js/2.bc77d50f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ade6d194e6c642d9566d",
+    "url": "/adminwebsite/static/js/main.7192f609.chunk.js"
+  },
+  {
+    "revision": "b5110656ef2b47055ca5",
+    "url": "/adminwebsite/static/js/runtime-main.2cec1359.js"
+  }
+]);
